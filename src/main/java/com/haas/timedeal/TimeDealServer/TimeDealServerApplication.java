def70be46e5466ea49b3c30813d0f5f59f6433e2
kimsangha617 <kimsangha617@gmail.com>
2023-02-27 14:05:10 +0900

    first commit

---
+++ src/main/java/com/haas/timedeal/TimeDealServer/TimeDealServerApplication.java
@@ -0,0 +1,13 @@
+package com.haas.timedeal.TimeDealServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeDealServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeDealServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haas/timedeal/TimeDealServer/TimeDealServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haas.timedeal.TimeDealServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeDealServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
